a236bf0547a08e67f40db5bd7c1ca0845140b587
HFS6666 <2264169272@qq.com>
2023-07-22 15:26:01 +0800

    tijiao

---
+++ src/main/java/com/jiawa/train/controller.java
@@ -0,0 +1,12 @@
+package com.jiawa.train;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class controller {
+    @GetMapping("/hello1")
+    public Object say(){
+        return "hello1111ddd1";
+    }
+}
