d47398f17facee96fd0ffc041332335ccc39cd86
HFS6666 <2264169272@qq.com>
2023-07-25 10:47:35 +0800

    增加短信验证码接口

--- member/src/main/java/com/wtbu/member/controller/TestController.java
+++ member/src/main/java/com/wtbu/member/controller/TestController.java
@@ -1,6 +1,7 @@
 package com.wtbu.member.controller;
 
 import com.wtbu.member.req.MemberRegisterReq;
+import com.wtbu.member.req.MemberSendcode;
 import com.wtbu.member.service.MemberService;
 import com.wtbu.train.common.resp.CommonResp;
 import jakarta.annotation.Resource;
@@ -22,5 +23,10 @@ public class TestController {
     @PostMapping("/register")
     public CommonResp<Long> register(@Valid MemberRegisterReq req){
         return memberService.register(req);
+    }
+    @PostMapping("/send-code")
+    public CommonResp<Long> sendcode(@Valid MemberSendcode req){
+         memberService.sendcode(req);
+         return new CommonResp<>();
     }
 }
--- member/src/main/java/com/wtbu/member/service/MemberService.java
+++ member/src/main/java/com/wtbu/member/service/MemberService.java
@@ -1,21 +1,26 @@
 package com.wtbu.member.service;
 
 import cn.hutool.core.collection.CollUtil;
+import cn.hutool.core.util.RandomUtil;
 import com.wtbu.member.domain.Member;
 import com.wtbu.member.domain.MemberExample;
 import com.wtbu.member.mapper.MemberMapper;
 import com.wtbu.member.req.MemberRegisterReq;
+import com.wtbu.member.req.MemberSendcode;
 import com.wtbu.train.common.exception.BusinessException;
 import com.wtbu.train.common.exception.BusinessExceptionEnum;
 import com.wtbu.train.common.resp.CommonResp;
 import com.wtbu.train.common.util.SnowUtil;
 import jakarta.annotation.Resource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class MemberService {
+    private static final Logger LOG = LoggerFactory.getLogger(MemberService.class);
     @Resource
     MemberMapper memberMapper;
 
@@ -40,6 +45,21 @@ public class MemberService {
         CommonResp<Long> commonResp=new CommonResp<>();
         commonResp.setContent(m.getId());
         return commonResp;
+    }
+    public void sendcode(MemberSendcode memberSendcode) {
+        MemberExample memberExample = new MemberExample();
+        String mobile=memberSendcode.getMobile();
+        memberExample.createCriteria().andMobileEqualTo(mobile);
+        List<Member> list = memberMapper.selectByExample(memberExample);
+        if (CollUtil.isEmpty(list)) {
+            LOG.info("手机号不存在，插入一条记录");
+            Member m = new Member();
+            m.setId(SnowUtil.getSnowflakeNextId());
+            m.setMobile(mobile);
+            memberMapper.insert(m);
+        }
+        String code=RandomUtil.randomString(4);
+        LOG.info("生成短信验证码:{}",code);
     }
 
 }
